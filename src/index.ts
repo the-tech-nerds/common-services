cf203025bf689bdd171db201fdf7b9e96df6de80
ARIFUL ISLAM <a.islam@stscapital.lt>
2020-11-17 00:52:55 +0600

    husky check

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { CacheModule } from './cache/cache.module';
-import { CacheService } from "./cache/cache.service";
+import { CacheService } from './cache/cache.service';
 
-export {
-    CacheModule,
-    CacheService
-};
+export { CacheModule, CacheService };
