8e6c39672a7fb360c0976dd55ea2dca2fdb58c2e
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2020-12-17 02:36:01 +0600

    GS-73 Added delete method for cache

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,14 +4,10 @@ import { CacheService } from './cache/cache.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly cacheService: CacheService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get('/hello')
   async getHello(): Promise<any> {
-    await this.cacheService.delete('user-token-3');
     return this.appService.getHello();
   }
 }
