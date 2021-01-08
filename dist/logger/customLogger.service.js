eeac77da028c1d7ec44d47de9d7b7d8511a188fc
rahimin <rahiminmyself@gmail.com>
2021-01-09 01:56:49 +0600

    logger path update

--- dist/logger/customLogger.service.js
+++ dist/logger/customLogger.service.js
@@ -3,36 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CustomLoggerService = void 0;
 const fs = require("fs");
 const moment = require("moment");
+const path_1 = require("path");
 class CustomLoggerService {
-    constructor(currentDate = moment().format('YYYY-MM-DD')) {
+    constructor(currentDate = moment().format('YYYY-MM-DD'), currentTime = moment().format('YYYY-MM-DD hh:mm:ss a'), logPath = path_1.default.join(__dirname, '../../../../logs/')) {
         this.currentDate = currentDate;
+        this.currentTime = currentTime;
+        this.logPath = logPath;
     }
     log(message) {
-        fs.writeFile(`./logs/log-${this.currentDate}.txt`, `LOG: ${message}`, err => {
+        fs.writeFile(`${this.logPath}log-${this.currentDate}.txt`, `LOG: Time: ${this.currentTime} :: ${message}`, err => {
             if (err) {
             }
         });
     }
     error(message) {
-        fs.writeFile(`./logs/error-${this.currentDate}.txt`, `ERROR: ${message}`, err => {
+        fs.writeFile(`${this.logPath}error-${this.currentDate}.txt`, `ERROR: Time: ${this.currentTime} :: ${message}`, err => {
             if (err) {
             }
         });
     }
     warn(message) {
-        fs.writeFile(`./logs/warning-${this.currentDate}.txt`, `WARNING: ${message}`, err => {
+        fs.writeFile(`${this.logPath}warning-${this.currentDate}.txt`, `WARNING: Time: ${this.currentTime} :: ${message}`, err => {
             if (err) {
             }
         });
     }
     debug(message) {
-        fs.writeFile(`./logs/debug-${this.currentDate}.txt`, `DEBUG: ${message}`, err => {
+        fs.writeFile(`${this.logPath}debug-${this.currentDate}.txt`, `DEBUG: Time: ${this.currentTime} :: ${message}`, err => {
             if (err) {
             }
         });
     }
     verbose(message) {
-        fs.writeFile(`./logs/verbos-${this.currentDate}.txt`, `VERBOSE: ${message}`, err => {
+        fs.writeFile(`${this.logPath}verbos-${this.currentDate}.txt`, `VERBOSE: Time: ${this.currentTime} :: ${message}`, err => {
             if (err) {
             }
         });
