303beee15b2e54102d0094ca4f4e1c5fec51e892
Sakibur Rahman <sakib.cse11.cuet@gmail.com>
2021-01-17 17:11:08 +0600

    Update auth services

--- src/guards/user/user.guard.ts
+++ src/guards/user/user.guard.ts
@@ -1,5 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { CacheService } from '../..';
 
 @Injectable()
-export class UserGuard extends AuthGuard('jwt') {}
+export class UserGuard extends AuthGuard('jwt') {
+  constructor(private readonly cacheService: CacheService) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): Promise<boolean> {
+    console.log(context);
+    return true;
+  }
+}
