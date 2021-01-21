58164609cce9dc2b7517eabb0995d2182a8878e0
Ariful Islam <arifulcuet52@gmail.com>
2021-01-22 01:32:31 +0600

    version

--- dist/guards/user/user.guard.d.ts
+++ dist/guards/user/user.guard.d.ts
@@ -1,9 +1,4 @@
-import { ExecutionContext } from '@nestjs/common';
-import { CacheService } from '../..';
 declare const UserGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
 export declare class UserGuard extends UserGuard_base {
-    private readonly cacheService;
-    constructor(cacheService: CacheService);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean>;
 }
 export {};
